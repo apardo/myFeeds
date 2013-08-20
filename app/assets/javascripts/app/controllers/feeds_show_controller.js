76af4afe9da1bab135bb3b80691ef998e802afb1
Antonio Pardo <apardo@alabs.org>
2013-08-20 16:09:08 -0500

    borrado de feeds

--- app/assets/javascripts/app/controllers/feeds_show_controller.js
+++ app/assets/javascripts/app/controllers/feeds_show_controller.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular.module('myFeeds')
-	.controller('FeedsShowCtrl', ['$scope', '$routeParams', 'FeedFactory', function($scope, $routeParams, FeedFactory) {
+	.controller('FeedsShowCtrl', ['$scope', '$location', '$routeParams', 'FeedFactory',
+        function($scope, $location, $routeParams, FeedFactory) {
 
         //Grab the feed from the server
         $scope.feed = FeedFactory.get({id: $routeParams.id});
-        
+
+        //Destroy method for deleting a feed
+    	$scope.destroy = function() {
+        	//Tell the server to remove the object
+        	FeedFactory.remove({id: $scope.feed.id}, function() {
+            	//If successful, remove it from our collection
+            	$scope.feed = [];
+                $location.path('/feeds');
+        	});
+    	}        
 	}]);
