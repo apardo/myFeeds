4077aaebf46c663c0a5243fc747eb8f78f3df2d9
Antonio Pardo <apardo@alabs.org>
2013-08-21 18:37:11 -0500

    get shared link data

--- app/assets/javascripts/app/controllers/items_show_controller.js
+++ app/assets/javascripts/app/controllers/items_show_controller.js
@@ -5,6 +5,23 @@ angular.module('myFeeds')
         function($scope, $routeParams, ItemFactory) {
 
         //Grab the item from the server
-        $scope.item = ItemFactory.get({id: $routeParams.id});
+        var getItem = ItemFactory.get({id: $routeParams.id}, function(result) {
+
+        	$scope.item = result;
+        	var feedUrl = encodeURIComponent($scope.item.url);
+
+        	$.ajax({
+        		type: 'GET',
+        		url: 'http://graph.facebook.com/',
+        		data: 'id=' + feedUrl,
+        		success: function(data) {
+        			console.log(data);
+        		},
+        		error: function() {
+        			console.log("ERROR");
+        		}
+        	});
+        });
+
     
 	}]);
